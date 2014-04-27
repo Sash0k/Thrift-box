a2d985064a916f6b4584095242f1217e027a1104
Alexander Vozzhennikov <Sash0k.V@yandex.ru>
2014-04-27 22:42:24 +0400

    Update widget every day at 0:00:00 by AlarmManager

--- thrift-box/src/main/java/ru/sash0k/thriftbox/Widget.java
+++ thrift-box/src/main/java/ru/sash0k/thriftbox/Widget.java
@@ -1,11 +1,15 @@
 package ru.sash0k.thriftbox;
 
+import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.widget.RemoteViews;
+
+import java.util.Calendar;
 
 import ru.sash0k.thriftbox.database.DB;
 
@@ -14,10 +18,22 @@ import ru.sash0k.thriftbox.database.DB;
  * Created by sash0k on 15.04.14.
  */
 public class Widget extends AppWidgetProvider {
+    public static String ACTION_AUTO_UPDATE_WIDGET = "ACTION_AUTO_UPDATE_WIDGET";
 
     @Override
     public void onEnabled(Context context) {
         super.onEnabled(context);
+        Intent intent = new Intent(Widget.ACTION_AUTO_UPDATE_WIDGET);
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(context, 0, intent, 0);
+
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, 0);
+        c.set(Calendar.MINUTE, 0);
+        c.set(Calendar.SECOND, 0);
+        c.set(Calendar.MILLISECOND, 1);
+
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmMgr.setRepeating(AlarmManager.RTC, c.getTimeInMillis(), AlarmManager.INTERVAL_DAY, alarmIntent);  // TODO: Android 4.4
     }
 
     @Override
@@ -36,6 +52,21 @@ public class Widget extends AppWidgetProvider {
     @Override
     public void onDisabled(Context context) {
         super.onDisabled(context);
+
+        Intent intent = new Intent(Widget.ACTION_AUTO_UPDATE_WIDGET);
+        AlarmManager alarmMgr = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        alarmMgr.cancel(PendingIntent.getBroadcast(context, 0, intent, 0));
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        super.onReceive(context, intent);
+
+        if (ACTION_AUTO_UPDATE_WIDGET.equals(intent.getAction())) {
+            AppWidgetManager widgetManager = AppWidgetManager.getInstance(context);
+            int[] widgets = widgetManager.getAppWidgetIds(new ComponentName(context, Widget.class));
+            for (int widget : widgets) updateWidget(context, widgetManager, widget);
+        }
     }
 
     public static void updateWidget(Context context, AppWidgetManager appWidgetManager, int widgetID) {
