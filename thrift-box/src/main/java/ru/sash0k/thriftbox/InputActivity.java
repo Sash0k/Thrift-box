a1d48ba94107b2359e32b710781218dadfdd3b3a
Alexander Vozzhennikov <Sash0k.V@yandex.ru>
2014-04-27 23:12:30 +0400

    ViewPager base implementation

--- thrift-box/src/main/java/ru/sash0k/thriftbox/InputActivity.java
+++ thrift-box/src/main/java/ru/sash0k/thriftbox/InputActivity.java
@@ -5,8 +5,6 @@ import android.appwidget.AppWidgetManager;
 import android.content.ComponentName;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -36,21 +34,6 @@ public class InputActivity extends Activity {
         super.onSaveInstanceState(state);
         state.putString(DB.VALUE, valueTV.getText().toString());
         state.putInt(DB.CATEGORY, categories.getSelected());
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     /**
