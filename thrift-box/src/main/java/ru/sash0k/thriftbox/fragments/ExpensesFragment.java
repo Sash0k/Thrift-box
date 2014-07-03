a314d1bd37d0347f28a7966bdf49d78c9c19b9b4
Alexander Vozzhennikov <Sash0k.V@yandex.ru>
2014-07-03 14:48:29 +0400

    expenses list: refactoring. Options menu disabled

--- thrift-box/src/main/java/ru/sash0k/thriftbox/fragments/ExpensesFragment.java
+++ thrift-box/src/main/java/ru/sash0k/thriftbox/fragments/ExpensesFragment.java
@@ -3,19 +3,27 @@ package ru.sash0k.thriftbox.fragments;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.BaseColumns;
+import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
+import android.support.v4.widget.CursorAdapter;
 
 import ru.sash0k.thriftbox.AdapterExpenses;
+import ru.sash0k.thriftbox.R;
 import ru.sash0k.thriftbox.database.DB;
 
 /**
  * Список расходов
  * Created by sash0k on 28.04.14.
  */
-public class ExpensesFragment extends BaseListFragment {
+public class ExpensesFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor>{
     private static final String TAG = "ExpensesFragment";
+    private static final int LOADER_ID = 1;
+
+    // Адаптер списка
+    protected CursorAdapter mAdapter;
+
 
     public static ExpensesFragment newInstance() {
         ExpensesFragment f = new ExpensesFragment();
@@ -44,8 +52,30 @@ public class ExpensesFragment extends BaseListFragment {
     // ============================================================================
 
     @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        mAdapter.swapCursor(data);
+
+        if (isResumed()) {
+            setListShown(true);
+        } else {
+            setListShownNoAnimation(true);
+        }
+    }
+    // ============================================================================
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        mAdapter.swapCursor(null);
+    }
+    // ============================================================================
+
+
+    @Override
     public void onActivityCreated(Bundle savedState) {
         super.onActivityCreated(savedState);
+        setEmptyText(getString(R.string.list_empty));
+        setHasOptionsMenu(false);
+        setListShown(false);
 
         LoaderManager lm = getLoaderManager();
         if (lm != null) lm.initLoader(LOADER_ID, null, this);
