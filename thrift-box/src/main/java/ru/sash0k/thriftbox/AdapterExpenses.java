b642a308288e73a37d4351a0a8019d302203d990
Alexander Vozzhennikov <Sash0k.V@yandex.ru>
2014-07-03 16:28:43 +0400

    expenses list: expand/collapse on day items

--- thrift-box/src/main/java/ru/sash0k/thriftbox/AdapterExpenses.java
+++ thrift-box/src/main/java/ru/sash0k/thriftbox/AdapterExpenses.java
@@ -2,10 +2,16 @@ package ru.sash0k.thriftbox;
 
 import android.content.Context;
 import android.database.Cursor;
-import android.support.v4.widget.CursorAdapter;
+import android.os.Bundle;
+import android.provider.BaseColumns;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
+import android.util.SparseIntArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CursorTreeAdapter;
 import android.widget.TextView;
 
 import ru.sash0k.thriftbox.database.DB;
@@ -14,28 +20,35 @@ import ru.sash0k.thriftbox.database.DB;
  * Адаптер списка расходов
  * Created by sash0k on 28.04.14.
  */
-public class AdapterExpenses extends CursorAdapter {
-    private static final String divider = " — ";
+public class AdapterExpenses extends CursorTreeAdapter {
     private final LayoutInflater mInflater;
     private final String[] categories;
 
-    public AdapterExpenses(Context context) {
-        super(context, null, 0);
+    private final Fragment mFragment;
+    protected final SparseIntArray mGroupMap;
+
+    public AdapterExpenses(Fragment fragment, Context context) {
+        super(null, context, true);
         this.mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         this.categories = context.getResources().getStringArray(R.array.categories);
+
+        this.mFragment = fragment;
+        this.mGroupMap = new SparseIntArray();
     }
     // ============================================================================
 
+    //Accessor method
+    public SparseIntArray getGroupMap() {
+        return mGroupMap;
+    }
+
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup viewGroup) {
+    protected View newGroupView(Context context, Cursor cursor, boolean b, ViewGroup viewGroup) {
         View view = mInflater.inflate(R.layout.listitem_expense, viewGroup, false);
         ViewHolder viewHolder = new ViewHolder();
         viewHolder.date = (TextView) view.findViewById(R.id.expense_date);
-        //viewHolder.category = (TextView) view.findViewById(R.id.expense_category);
         viewHolder.value = (TextView) view.findViewById(R.id.expense_value);
-
         viewHolder.date_col = cursor.getColumnIndexOrThrow(DB.DATE);
-        //viewHolder.category_col = cursor.getColumnIndexOrThrow(DB.CATEGORY);
         viewHolder.value_col = cursor.getColumnIndexOrThrow(DB.VALUE);
         view.setTag(viewHolder);
         return view;
@@ -43,31 +56,62 @@ public class AdapterExpenses extends CursorAdapter {
     // ============================================================================
 
     @Override
-    public void bindView(View view, Context context, Cursor cursor) {
+    protected void bindGroupView(View view, Context context, Cursor cursor, boolean b) {
         ViewHolder viewHolder = (ViewHolder) view.getTag();
         viewHolder.date.setText(cursor.getString(viewHolder.date_col));
-        //viewHolder.category.setText(divider + categories[cursor.getInt(viewHolder.category_col)]);
         viewHolder.value.setText(Utils.formatValue(cursor.getLong(viewHolder.value_col)) + context.getString(R.string.ruble));
     }
     // ============================================================================
 
     @Override
-    public String getItem(int position) {
-        String result = null;
-        Cursor cursor = getCursor();
-        if ((cursor != null) && (cursor.moveToPosition(position))) {
-            result = cursor.getString(cursor.getColumnIndex(DB.VALUE));
-            cursor.close();
+    protected View newChildView(Context context, Cursor cursor, boolean b, ViewGroup viewGroup) {
+        View view = mInflater.inflate(R.layout.listitem_expense, viewGroup, false);
+        ViewHolder viewHolder = new ViewHolder();
+        viewHolder.category = (TextView) view.findViewById(R.id.expense_category);
+        viewHolder.value = (TextView) view.findViewById(R.id.expense_value);
+
+        viewHolder.category_col = cursor.getColumnIndexOrThrow(DB.CATEGORY);
+        viewHolder.value_col = cursor.getColumnIndexOrThrow(DB.VALUE);
+        view.setTag(viewHolder);
+        return view;
+    }
+    // ============================================================================
+
+    @Override
+    protected void bindChildView(View view, Context context, Cursor cursor, boolean b) {
+        ViewHolder viewHolder = (ViewHolder) view.getTag();
+        viewHolder.category.setText('\t' + categories[cursor.getInt(viewHolder.category_col)]);
+        viewHolder.value.setText(Utils.formatValue(cursor.getLong(viewHolder.value_col)) + context.getString(R.string.ruble));
+    }
+    // ============================================================================
+
+    @Override
+    protected Cursor getChildrenCursor(Cursor cursor) {
+        final int groupPos = cursor.getPosition();
+        final int groupId = cursor.getInt(cursor.getColumnIndex(BaseColumns._ID));
+        final String date  = cursor.getString(cursor.getColumnIndex(DB.DATE));
+        Utils.log("pos = " + groupPos);
+        Utils.log("date = " + date);
+
+        mGroupMap.put(groupId, groupPos);
+        Bundle args = new Bundle();
+        args.putString(DB.DATE, date);
+        LoaderManager lm = mFragment.getLoaderManager();
+        Loader loader = lm.getLoader(groupId);
+        if (loader != null && !loader.isReset()) {
+            lm.restartLoader(groupId, args, (LoaderManager.LoaderCallbacks<Object>) mFragment);
+        } else {
+            lm.initLoader(groupId, args, (LoaderManager.LoaderCallbacks<Object>) mFragment);
         }
-        return result;
+        return null;
     }
     // ============================================================================
 
     private static class ViewHolder {
+        private TextView category;
         private TextView date;
-        // private TextView category;
         private TextView value;
-        private int date_col, value_col;
+        private int date_col, category_col, value_col;
     }
     // ============================================================================
 }
--- thrift-box/src/main/java/ru/sash0k/thriftbox/fragments/ExpensesFragment.java
+++ thrift-box/src/main/java/ru/sash0k/thriftbox/fragments/ExpensesFragment.java
@@ -3,26 +3,28 @@ package ru.sash0k.thriftbox.fragments;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.BaseColumns;
-import android.support.v4.app.ListFragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.CursorLoader;
 import android.support.v4.content.Loader;
 import android.support.v4.widget.CursorAdapter;
+import android.util.SparseIntArray;
 
 import ru.sash0k.thriftbox.AdapterExpenses;
 import ru.sash0k.thriftbox.R;
+import ru.sash0k.thriftbox.Utils;
 import ru.sash0k.thriftbox.database.DB;
 
 /**
  * Список расходов
  * Created by sash0k on 28.04.14.
  */
-public class ExpensesFragment extends ListFragment implements LoaderManager.LoaderCallbacks<Cursor>{
+public class ExpensesFragment extends ExpandableListFragment implements LoaderManager.LoaderCallbacks<Cursor>{
     private static final String TAG = "ExpensesFragment";
-    private static final int LOADER_ID = 1;
+    private static final int LOADER_ID = -1;
+    public static final int LOADER_DETAILS = 2;
 
     // Адаптер списка
-    protected CursorAdapter mAdapter;
+    protected AdapterExpenses mAdapter;
 
 
     public static ExpensesFragment newInstance() {
@@ -38,34 +40,54 @@ public class ExpensesFragment extends ListFragment implements LoaderManager.Load
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        mAdapter = new AdapterExpenses(getActivity());
+        mAdapter = new AdapterExpenses(this, getActivity());
         setListAdapter(mAdapter);
     }
     // ============================================================================
 
     @Override
     public Loader<Cursor> onCreateLoader(int id, Bundle args) {
-        final String[] columns = {BaseColumns._ID, "SUM(" + DB.VALUE + ") AS " + DB.VALUE, DB.DATE};
-        final String selection = DB.DATE + " = " + DB.DATE + " GROUP BY (" + DB.DATE + ")"; // hack: GROUP BY
-        return new CursorLoader(getActivity(), DB.getUri(DB.EXPENSES_VIEW), columns, selection, null, DB.TIMESTAMP + " DESC");
+        if (id == LOADER_ID) {
+            final String[] columns = {BaseColumns._ID, "SUM(" + DB.VALUE + ") AS " + DB.VALUE, DB.DATE};
+            final String selection = DB.DATE + " = " + DB.DATE + " GROUP BY (" + DB.DATE + ")"; // hack: GROUP BY
+            return new CursorLoader(getActivity(), DB.getUri(DB.EXPENSES_VIEW), columns, selection, null, DB.TIMESTAMP + " DESC");
+        } else {
+            final String date = args.getString(DB.DATE);
+            return new CursorLoader(getActivity(), DB.getUri(DB.EXPENSES_VIEW), null, DB.DATE + "=?", new String[]{date}, DB.TIMESTAMP + " DESC");
+        }
     }
     // ============================================================================
 
     @Override
     public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
-        mAdapter.swapCursor(data);
+        final int id = loader.getId();
 
         if (isResumed()) {
             setListShown(true);
         } else {
             setListShownNoAnimation(true);
         }
+
+        if (id != LOADER_ID) {
+            // child cursor
+            if (!data.isClosed()) {
+                Utils.log("data.getCount() " + data.getCount());
+                SparseIntArray groupMap = mAdapter.getGroupMap();
+                try {
+                    int groupPos = groupMap.get(id);
+                    Utils.log("onLoadFinished() for groupPos " + groupPos);
+                    mAdapter.setChildrenCursor(groupPos, data);
+                } catch (NullPointerException e) {
+                    Utils.log("Adapter expired, try again on the next query: " + e.getMessage());
+                }
+            }
+        } else mAdapter.changeCursor(data);
     }
     // ============================================================================
 
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
-        mAdapter.swapCursor(null);
+        mAdapter.changeCursor(null);
     }
     // ============================================================================
 
